44f2e6fd5c9722e9e36a377968e869ee6b941992
Destined Codes <onesmusmwandawiro531@gmail.com>
2026-04-22 20:41:15 +0300

    Add merge migration for transaction ownership

---
+++ core/migrations/0018_merge_0012_transaction_created_by_0017_activitylog_updates.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0012_transaction_created_by'),
+        ('core', '0017_rename_core_activi_user_id_a5e86b_idx_core_activi_user_id_81b1f1_idx_and_more'),
+    ]
+
+    operations = []
