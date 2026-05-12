b5cdf1b0daeea319cfa14b2205f86bf199e01348
DestinedCodes <onesmusmwandawiro531@gmail.com>
2026-05-12 12:59:26 +0300

    Add progressive web app support

--- CyberPoa/urls.py
+++ CyberPoa/urls.py
@@ -18,8 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.cache import never_cache
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path(
+        'service-worker.js',
+        never_cache(TemplateView.as_view(
+            template_name='service-worker.js',
+            content_type='application/javascript',
+        )),
+        name='service_worker',
+    ),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
---
+++ templates/service-worker.js
@@ -0,0 +1,55 @@
+{% load static %}
+const CACHE_NAME = 'meneja360-pwa-v1';
+const APP_SHELL = [
+    '{% static "manifest.webmanifest" %}',
+    '{% static "css/style.css" %}',
+    '{% static "js/pwa.js" %}',
+    '{% static "img/cyberpoa-logo.svg" %}',
+    '{% static "offline.html" %}'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames
+                .filter((cacheName) => cacheName !== CACHE_NAME)
+                .map((cacheName) => caches.delete(cacheName))
+        ))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    const request = event.request;
+
+    if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) {
+        return;
+    }
+
+    if (request.mode === 'navigate') {
+        event.respondWith(
+            fetch(request).catch(() => caches.match('{% static "offline.html" %}'))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(request).then((cachedResponse) => (
+            cachedResponse || fetch(request).then((networkResponse) => {
+                const shouldCache = request.url.includes('/static/');
+                if (shouldCache && networkResponse.ok) {
+                    const responseToCache = networkResponse.clone();
+                    caches.open(CACHE_NAME).then((cache) => cache.put(request, responseToCache));
+                }
+                return networkResponse;
+            })
+        ))
+    );
+});
